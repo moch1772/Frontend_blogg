33bbc129c2d0782fdd805ca755c5708c1fa777e2
Gustav <gustavc24@gmail.com>
2020-12-04 10:25:28 +0100

    liten frontend

--- Frontend/index.php
+++ Frontend/index.php
@@ -3,13 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
     <title>Document</title>
 </head>
 <body>
     <?php
-        echo "FEL";
-        echo "</br>";
-        echo "PS: Karl har gjort mig besviken";
+        include_once ""
     ?>
+    <div class="content">
+        <div class="foreground">
+            <div class="billboard"></div>
+            <div class="title"><h1>Service Titel</h1></div>
+        </div>
+    </div>
 </body>
 </html>
