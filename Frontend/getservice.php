39f6c727873a3fdde120be72d426db04384a58e0
huggman34 <hugo.nyman@elev.ga.ntig.se>
2020-12-07 16:28:40 +0100

    sådär

--- Frontend/getservice.php
+++ Frontend/getservice.php
@@ -1,7 +1,25 @@
 <?php
+function serviceTitle($API,$serviceID){
+    $ch=curl_init();
 
+    $url= "http://sko.te4-ntig.se/wider/api/pages/read_single_service.php?API=$API&serviceID=$serviceID";
+    curl_setopt($ch, CURLOPT_URL, $url);
 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    
+    $output = curl_exec($ch);
+    $s=strpos($output,'","serviceD');
+    $d=strpos($output,'Title":"');
+    $d+=8;
+    $p=$s-$d;
+    
+    $serTitle=substr($output,$d,$p);
+    echo$serTitle;
+    
+}
 function servicePage($API,$serviceID){
+   
+
 $url = "http://sko.te4-ntig.se/wider/api/pages/read_page_service.php?API=$API&serviceID=$serviceID";
 
                        
@@ -19,6 +37,7 @@ $output = file_get_contents($url);
          $ul = "http://sko.te4-ntig.se/wider/api/pages/read_post_page.php?API=$API&pageID=$pageID";
          $outpt = file_get_contents($ul);
         $redPost=json_decode($outpt,true);
+        
         $cont=0;
         foreach($redPost as $red){
             $cont +=count($red);
@@ -28,17 +47,34 @@ $output = file_get_contents($url);
             $postID=$redPost['posts'][$s]['postID'];
             $postTitel=$redPost['posts'][$s]['postTitle'];
             $postText=$redPost['posts'][$s]['pText'];
+            $imageURL=$redPost['posts'][$s]['imageURL'];
             
         
         echo"<div class='postTitle'><h2>$postTitel</h2></div>
             <div class='text'>$postText</div>
             <img src='../img/360.jpg' class='img'>
             <img src='../icon/25360.png' class='icon'>";
+           
          }
         }
         
         
      
             
+    }
+    function readComment($API,$pageID){
+        $ulc = "http://sko.te4-ntig.se/wider/api/comment/read_page_comment.php?API=$API&pageID=$pageID";
+        $como= file_get_contents($ulc);
+       $redComment=json_decode($como,true);
+
+       $count=0;
+       foreach($redComment as $red){
+           $count +=count($red);
+       }
+       for ($i=0; $i < $count; $i++) { 
+        $comment=$redComment['comment'][$i]['cText'];
+        echo $comment;
+       }
+       
     }
 ?>
