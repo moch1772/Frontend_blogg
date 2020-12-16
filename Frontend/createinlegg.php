b1441e424bb39f1d62c491c8593ea3bb75ee3127
moch1772 <ziggs1772@gmail.com>
2020-12-16 11:38:21 +0100

    create Inlegg can choose style

--- Frontend/createinlegg.php
+++ Frontend/createinlegg.php
@@ -11,13 +11,24 @@ $bold="bold";
     echo"<form action='createinlegg.php' method='post' enctype='multipart/form-data'>
 
 <input type='hidden' name='font' value='$font'>
-<input type='text' name='postTitle'>
 <input type='button' value='add text box' onclick='newpost(pp)'>
 
 <div id='pp'>
-<input type='file' name='image' accept='image/x-png,image/gif,image/jpeg'><input type='text' name='subtitle[]'><input type='text' name='subtext[]'>
+<input type='text' name='postTitle' placeholder='page title'>
+<input type='text' name='subtitle[]' placeholder='post title'>
+<input type='text' name='subtext[]' placeholder='post text'>
+<input type='file' name='image' accept='image/x-png,image/gif,image/jpeg'>
 </div>
-
+<input type='radio' id='picLeft' name='picControl' value='picLeft'>
+<label for='picLeft'>Image to the left</label><br>
+<input type='radio' id='picRight' name='picControl' value='picRight'>
+<label for='picRight'>Image to the right</label><br>
+<input type='radio' id='textLeft' name='picControl' value='textLeft' checked='checked'>
+<label for='textLeft'>No image text alaignd to the left</label>
+<br>
+<input type='radio' id='textRight' name='picControl' value='textRight'>
+<label for='textRight'>No image text alaignd to the right</label>
+<br>
 <input type='submit' name='submit' value='submit'>
 </form>";
 if(isset($_POST['submit'])){
@@ -25,6 +36,7 @@ if(isset($_POST['submit'])){
     $postText=$_POST['subtext'];
     $metaTag="hugo";
     $pageTitle="smothe";
+    $style=$_POST['picControl'];
 
     $username="karl";
     $API="RRmjdNWZuAeDqhEPrCWT";
@@ -33,37 +45,53 @@ if(isset($_POST['submit'])){
     $image=$_FILES['image']['name'];
 
     
+    if($style=='picLeft' || $style=='picRight'){
+        if($style=='picLeft'){
+            $target_dir = "../img/left";
+        }else{
+            $target_dir = "../img/right";
+        }
     
-    $target_dir = "../img/";
-    $imgName=basename($_FILES["image"]["name"]);
-    $target_file = $target_dir . basename($_FILES["image"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        $imgName=basename($_FILES["image"]["name"]);
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-    // Kollar om filen redan finns
-    if ($uploadOk==1 && file_exists($target_file)) {
-    echo "Sorry, file already exists.<br>";
-    $uploadOk = 0;
-    }
-    
-    if ($uploadOk==1 && $_FILES["image"]["size"] > 5000000) {
-        echo "Sorry, your file is too large.<br>";
-        $uploadOk = 0;
-    }
-    // Kollar om $uploadOk är 0 eller inte
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-    //die;
-    //header('Location: ' . $_SERVER['HTTP_REFERER']);
-  // Laddar upp filen
-  } else {
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-      echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.<br>";
+        // Kollar om filen redan finns
+        if ($uploadOk==1 && file_exists($target_file)) {
+            echo "Sorry, file already exists.<br>";
+            $uploadOk = 0;
+        }
+        
+        if ($uploadOk==1 && $_FILES["image"]["size"] > 5000000) {
+            echo "Sorry, your file is too large.<br>";
+            $uploadOk = 0;
+        }
+        // Kollar om $uploadOk är 0 eller inte
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+            //die;
+            //header('Location: ' . $_SERVER['HTTP_REFERER']);
+            // Laddar upp filen
+        } else {
+            if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+                echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.<br>";
+            }
+        }
     }
 }
-      
-    $image=array("../img/".$image);
-}
+    if($style=='picLeft'){
+        $image=array("../img/left".$image);
+    }
+    if($style=='picRight'){
+        $image=array("../img/right".$image);
+    }
+    if($style=='textLeft'){
+        $image=array("../img/left");
+    }
+    if($style=='textRight'){
+        $image=array("../img/right");
+    }
     $count=0;
     foreach ($postTitle as $po) {
         $count++;
